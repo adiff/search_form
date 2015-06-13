729c6e69b4722ac2fc091c7d464277a743be5754
adiff <adifragman@gmail.com>
2015-06-13 18:30:23 -0400

    First commit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,6 @@
 class PeopleController < ApplicationController
         def index
                 @people = Person.find_by(firstname: params[:search]) if params[:search].present?
-                @people = Person.all
         end
 
         def new
